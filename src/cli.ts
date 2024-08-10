1fdda8e12212963576cd60edbb976a1785db9a54
Katumbela <ja3328173@gmail.com>
2024-08-10 16:25:44 +0100

    commit 2 - src/cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -6,31 +6,33 @@ import path from 'path';
 const targetDir = '.';
 
 function run() {
-  try { 
-    process.chdir(path.resolve(targetDir));
+    try {
+        process.chdir(path.resolve(targetDir));
 
-    let count = 1;
- 
-    const untrackedFiles = execSync('git ls-files --others --exclude-standard').toString().trim().split('\n');
-    untrackedFiles.forEach(file => {
-      if (file) {
-        console.log(`Adding untracked file ${file}`);
-        execSync(`git add "${file}"`);
-        execSync(`git commit -m "commit ${count++} - ${file}"`);
-      }
-    });
- 
-    const modifiedFiles = execSync('git diff --name-only').toString().trim().split('\n');
-    modifiedFiles.forEach(file => {
-      if (file) {
-        console.log(`Adding modified file ${file}`);
-        execSync(`git add "${file}"`);
-        execSync(`git commit -m "commit ${count++} - ${file}"`);
-      }
-    });
-  } catch (error) {
-    console.error('Error executing commands:', (error as Error).message);
-  }
+        let count = 1;
+
+        const untrackedFiles = execSync('git ls-files --others --exclude-standard').toString().trim().split('\n');
+        untrackedFiles.forEach(file => {
+            if (file) {
+                console.log(`📁  Adicionando ficheiros nao rastreados ${file}`);
+                execSync(`git add "${file}"`);
+                execSync(`git commit -m "commit ${count++} - ${file}"`);
+                console.log(`✅ Ficheiro nao rastreados commitados ${file}`);
+            }
+        });
+
+        const modifiedFiles = execSync('git diff --name-only').toString().trim().split('\n');
+        modifiedFiles.forEach(file => {
+            if (file) {
+                console.log(`📝 Adicionando ficheiros modificados ${file}`);
+                execSync(`git add "${file}"`);
+                execSync(`git commit -m "commit ${count++} - ${file}"`);
+                console.log(`✅ Ficheiros modificados Commitados ${file}`);
+            }
+        });
+    } catch (error) {
+        console.error('Erro ao executar seus commits:', (error as Error).message);
+    }
 }
 
 run();
