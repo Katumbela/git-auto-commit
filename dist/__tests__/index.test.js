842087657d7d2011e9c9e395673fafc1426bdce0
Katumbela <ja3328173@gmail.com>
2024-08-10 16:10:04 +0100

    commit 11 - dist/__tests__/index.test.js

--- dist/__tests__/index.test.js
+++ dist/__tests__/index.test.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/__tests__/index.test.ts
 const index_1 = require("../index");
 const child_process_1 = require("child_process");
 jest.mock('child_process', () => ({
@@ -14,14 +13,18 @@ describe('GitCommitLib', () => {
     beforeEach(() => {
         gitLib = new index_1.GitCommitLib();
     });
-    test('should add and commit untracked files', () => {
-        gitLib.testAddUntrackedFiles();
-        expect(child_process_1.execSync).toHaveBeenCalledWith(expect.stringContaining('git add'));
-        expect(child_process_1.execSync).toHaveBeenCalledWith(expect.stringContaining('git commit -m'));
+    test('should handle errors when adding untracked files', () => {
+        child_process_1.execSync.mockImplementationOnce(() => {
+            throw new Error('Simulated error');
+        });
+        expect(() => gitLib.testAddUntrackedFiles()).not.toThrow();
+        expect(console.error).toHaveBeenCalledWith('Error adding untracked files:', 'Simulated error');
     });
-    test('should add and commit modified files', () => {
-        gitLib.testAddModifiedFiles();
-        expect(child_process_1.execSync).toHaveBeenCalledWith(expect.stringContaining('git add'));
-        expect(child_process_1.execSync).toHaveBeenCalledWith(expect.stringContaining('git commit -m'));
+    test('should handle errors when adding modified files', () => {
+        child_process_1.execSync.mockImplementationOnce(() => {
+            throw new Error('Simulated error');
+        });
+        expect(() => gitLib.testAddModifiedFiles()).not.toThrow();
+        expect(console.error).toHaveBeenCalledWith('Error adding modified files:', 'Simulated error');
     });
 });
