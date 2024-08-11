0991dce2151901bf38f2ba2a2658b424e1b24896
Katumbela <ja3328173@gmail.com>
2024-08-11 16:36:15 +0100

    fix: Alterações gerais ou refatoração no arquivo src/cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -18,43 +18,43 @@ function generateMessageFromDiff(diff: string, file: string): string {
     const messages: string[] = [];
 
     // Detecção de adição de função
-    if (/function\s+(\w+)/.test(diff)) {
+    if (/^\+?\s*function\s+(\w+)/.test(diff)) {
         const functionName = diff.match(/function\s+(\w+)/)?.[1];
         messages.push(`Criação da função ${functionName} no arquivo ${file}`);
     }
 
     // Detecção de criação de botão
-    if (/<button/.test(diff)) {
+    if (/^\+?\s*<button/.test(diff)) {
         messages.push(`Criação de um botão no arquivo ${file}`);
     }
 
     // Detecção de criação de imagem
-    if (/<img/.test(diff)) {
+    if (/^\+?\s*<img/.test(diff)) {
         messages.push(`Criação de uma imagem no arquivo ${file}`);
     }
 
     // Detecção de estilização CSS
-    if (/background-color|color|font-size/.test(diff)) {
+    if (/^\+?\s*(background-color|color|font-size)/.test(diff)) {
         messages.push(`Estilização de CSS no arquivo ${file}`);
     }
 
     // Detecção de adição de comentário
-    if (/^\s*\/\/\s/.test(diff)) {
+    if (/^\+?\s*\/\/\s/.test(diff)) {
         messages.push(`Adição de um comentário no arquivo ${file}`);
     }
 
     // Detecção de adição de log no console
-    if (/console\.log/.test(diff)) {
+    if (/^\+?\s*console\.log/.test(diff)) {
         messages.push(`Adição de log no console no arquivo ${file}`);
     }
 
     // Detecção de adição de importação
-    if (/^\+\s*import/.test(diff)) {
+    if (/^\+?\s*import/.test(diff)) {
         messages.push(`Adição de importação no arquivo ${file}`);
     }
 
     // Detecção de manipulação de DOM
-    if (/document\.querySelector|document\.getElementById/.test(diff)) {
+    if (/^\+?\s*(document\.querySelector|document\.getElementById)/.test(diff)) {
         messages.push(`Manipulação de DOM no arquivo ${file}`);
     }
 
@@ -75,40 +75,40 @@ function generateMessageFromDiff(diff: string, file: string): string {
     }
 
     // Detecção de alteração de texto
-    if (/text/.test(diff)) {
+    if (/^\+?\s*text/.test(diff)) {
         messages.push(`Alteração de texto no arquivo ${file}`);
     }
 
     // Detecção de criação de constante ou variável
-    if (/const\s+\w+\s*=/.test(diff)) {
+    if (/^\+?\s*const\s+\w+\s*=/.test(diff)) {
         messages.push(`Criação de uma constante no arquivo ${file}`);
     }
-    if (/let\s+\w+\s*=|var\s+\w+\s*=/.test(diff)) {
+    if (/^\+?\s*(let|var)\s+\w+\s*=/.test(diff)) {
         messages.push(`Criação de uma variável no arquivo ${file}`);
     }
 
     // Detecção de alteração em componente React
-    if (/React\.Component|function\s+\w+\(.*\)\s*{/.test(diff)) {
+    if (/^\+?\s*(React\.Component|function\s+\w+\(.*\)\s*{)/.test(diff)) {
         messages.push(`Alteração em componente React no arquivo ${file}`);
     }
 
     // Detecção de adição de novo import
-    if (/^import\s+\w+/.test(diff)) {
+    if (/^\+?\s*import\s+\w+/.test(diff)) {
         messages.push(`Adição de novo import no arquivo ${file}`);
     }
 
     // Detecção de mudança de texto em elemento HTML
-    if (/>\s*\w+.*<\/\w+>/.test(diff)) {
+    if (/^\+?\s*>\s*\w+.*<\/\w+>/.test(diff)) {
         messages.push(`Mudança de texto em um elemento HTML no arquivo ${file}`);
     }
 
     // Detecção de criação de novo método de classe
-    if (/^\s*(public|private|protected)?\s*\w+\s*\(.*\)\s*{/.test(diff)) {
+    if (/^\+?\s*(public|private|protected)?\s*\w+\s*\(.*\)\s*{/.test(diff)) {
         messages.push(`Criação de um novo método de classe no arquivo ${file}`);
     }
 
     // Detecção de adição de estilo inline
-    if (/style=\{[^}]+\}/.test(diff)) {
+    if (/^\+?\s*style=\{[^}]+\}/.test(diff)) {
         messages.push(`Adição de um novo estilo inline no arquivo ${file}`);
     }
 
@@ -128,22 +128,22 @@ function generateMessageFromDiff(diff: string, file: string): string {
     }
 
     // Detecção de mudança na estrutura de pastas
-    if (/^mv\s+/.test(diff)) {
+    if (/^\s*mv\s+/.test(diff)) {
         messages.push(`Mudança na estrutura de pastas no arquivo ${file}`);
     }
 
     // Detecção de alteração em funções de manipulação de eventos
-    if (/addEventListener | onClick | onChange /.test(diff)) {
+    if (/^\+?\s*(addEventListener|onClick|onChange)/.test(diff)) {
         messages.push(`Alteração em funções de manipulação de eventos no arquivo ${file}`);
     }
 
     // Detecção de alteração de conteúdo em array ou objeto
-    if (/\w+\s*=\s*\[.*\]|\w+\s*=\s*{.*}/.test(diff)) {
+    if (/^\+?\s*\w+\s*=\s*\[.*\]|\w+\s*=\s*{.*}/.test(diff)) {
         messages.push(`Alteração de conteúdo em um array ou objeto no arquivo ${file}`);
     }
 
     // Detecção de criação de testes unitários
-    if (/describe\(|it\(|test\(/.test(diff)) {
+    if (/^\+?\s*(describe\(|it\(|test\()/.test(diff)) {
         messages.push(`Criação de testes unitários no arquivo ${file}`);
     }
 
@@ -154,6 +154,7 @@ function generateMessageFromDiff(diff: string, file: string): string {
 
     return messages.join('; ');
 }
+
 
 function run() {
     try {
